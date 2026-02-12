26511341a123c54ea5e24b64c87a19ca39b593cc
BSTRD <107222627+C0D3Hermit@users.noreply.github.com>
2026-02-12 12:52:21 -0800

    Update resource URL for StableLoader.js

--- branches/StableLoader.js
+++ branches/StableLoader.js
@@ -1,6 +1,6 @@
 document.addEventListener('corru_entered', ()=>{
     if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
-    addResources(["https://github.com/C0D3Hermit/Murky-Waters-Modpack/blob/mod/StableMurky.js"])
+    addResources(["https://raw.githubusercontent.com/C0D3Hermit/Murky-Waters-Modpack/refs/heads/mod/StableMurky.js"])
     addResources(["https://adenator.neocities.org/corrumods/literallyTooManyHumors.js"])
     }
 })
