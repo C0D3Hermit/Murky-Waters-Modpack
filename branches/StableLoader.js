8bcb8006ea30f35c8e2220a58b5cd1ee364cd5ee
BSTRD <107222627+C0D3Hermit@users.noreply.github.com>
2026-02-12 11:50:49 -0800

    Implemented loader for the mod
    yay

---
+++ branches/StableLoader.js
@@ -0,0 +1,6 @@
+document.addEventListener('corru_entered', ()=>{
+    if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
+    addResources(["https://github.com/C0D3Hermit/Murky-Waters-Modpack/blob/mod/StableMurky.js"])
+    addResources(["https://adenator.neocities.org/corrumods/literallyTooManyHumors.js"])
+    }
+})
